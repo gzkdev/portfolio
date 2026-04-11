0285b1bef057ac9201fa1f2e25f50c3cd0a2536e
Ezihe Godswill <ezihegodswill01@gmail.com>
2026-04-11 16:19:47 +0100

    chore: update prettier configuration

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,6 +1,6 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 import { highlight } from 'sugar-high';
-import { cn } from '@/lib/utils';
 
 interface CodeProps extends React.HTMLAttributes<HTMLElement> {
   code?: string;
