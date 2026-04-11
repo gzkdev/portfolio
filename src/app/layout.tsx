54863b55487f281434d5091b6d1246f6ba4018e2
Ezihe Godswill <ezihegodswill01@gmail.com>
2026-04-11 16:20:13 +0100

    style: simplify global styles and switch to Geist fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,31 @@
-import { Inter, JetBrains_Mono } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { cn } from '@/lib/utils';
-import { Footer } from '@/components/footer';
 import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
+const fontSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
-const fontMono = JetBrains_Mono({
+const fontMono = Geist_Mono({
   subsets: ['latin'],
   variable: '--font-mono',
 });
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="en"
-      className={cn(fontMono.variable, inter.variable)}
+      className={cn(fontMono.variable, fontSans.variable)}
       suppressHydrationWarning
     >
       <body>
         <ThemeProvider enableSystem defaultTheme="light">
           {children}
         </ThemeProvider>
-        <Footer />
       </body>
     </html>
   );
