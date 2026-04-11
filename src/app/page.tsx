3e80e8a43a966b1a399644ca469964896cb6d2fa
Ezihe Godswill <ezihegodswill01@gmail.com>
2026-04-11 16:20:22 +0100

    feat: redesign home page and remove unused home sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,46 @@
-import { PROJECTS } from '@/lib/constants';
-import { getBlogPosts, getExperiments } from '@/lib/mdx';
-import { BlogSection } from '@/components/home/blog-section';
-import { ExperimentsSection } from '@/components/home/experiments-section';
-import { IntroSection } from '@/components/home/intro-section';
-import { ProjectsSection } from '@/components/home/projects-section';
-
-export default async function Home() {
-  const [blogPosts, experiments] = await Promise.all([
-    getBlogPosts(),
-    getExperiments(),
-  ]);
-
+export default async function Page() {
   return (
-    <main className="flex flex-col gap-16">
-      <IntroSection />
-      <ProjectsSection data={PROJECTS} />
-      <ExperimentsSection data={experiments} />
-      <BlogSection data={blogPosts} />
+    <main className="mx-auto flex max-w-xl flex-col gap-16">
+      <section className="space-y-4">
+        <h1 className="font-semibold tracking-tight">Godswill Ezihe</h1>
+
+        <div className="space-y-4">
+          <p>
+            Frontend Engineer building onchain and AI-native apps and tools.
+          </p>
+
+          <p>
+            I believe great engineering is about architecting and building apps
+            that handle tough circumstances gracefully without sacrificing user
+            experience.
+          </p>
+
+          <p>
+            Previously, I worked at{' '}
+            <a
+              href="http://x.com/IthacaProtocol"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ithaca Protocol
+            </a>
+            . Today, I'm actively learning more about AI, Algorithms and UI
+            engineering.
+          </p>
+
+          <p>
+            You can reach me on{' '}
+            <a
+              href="http://x.com/ezihegodswill"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              X/Twitter
+            </a>{' '}
+            or via <a href="mailto:ezihegodswill01@gmail.com">Email</a>
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
